599d2996fa11f0c79216b25bc79be368c27fbfcd
saerem21 <nemessisdds>
2014-01-03 16:20:23 +0900

    슬라이더에 따라 색상이 선택되는 예제입니다..
    슬라이더에 따라 색상이 선택되는 예제입니다..

--- ColorTest/ViewController.m
+++ ColorTest/ViewController.m
@@ -9,10 +9,45 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIView *colorView;
+@property (strong, nonatomic) IBOutlet UISlider *red;
+@property (strong, nonatomic) IBOutlet UISlider *green;
+@property (strong, nonatomic) IBOutlet UISlider *blue;
+
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *indi1;
+- (IBAction)rgbPicker:(id)sender;
 
 @end
 
 @implementation ViewController
+
+- (IBAction)rgbPicker:(UISlider *)sender {
+    if([self.indi1 isAnimating]){
+        [self.indi1 stopAnimating];
+    }
+    self.colorView.backgroundColor = [UIColor colorWithRed:self.red.value green:self.green.value blue:self.blue.value alpha:1.0];
+    
+}
+- (IBAction)onOffSwitch:(UISwitch *)sender {
+    BOOL isOn = sender.on;
+    
+    self.red.enabled =isOn;
+    self.green.enabled =isOn;
+    self.blue.enabled =isOn;
+    
+    if(isOn){
+        [self.indi1 startAnimating];
+        [self performSelector:@selector(rgbPicker:) withObject:nil afterDelay:1.0];
+    }
+    else{
+        self.colorView.backgroundColor = [ UIColor grayColor];
+    }
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [self rgbPicker:nil];
+}
 
 - (void)viewDidLoad
 {
